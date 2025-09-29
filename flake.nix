1f1911f3b97f58cdebfe7c553862f98aa77e149a
timmyjinks <tysonjjenkins.github@gmail.com>
2025-09-28 23:13:55 -0600

    Change flake version

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "TYSONCLOUD Server Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... } @inputs: {
-    nixosConfigurations.tysoncloud = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./tysoncloud.nix
-      ];
+  outputs = { self, nixpkgs, ... }:
+    {
+      nixosConfigurations.tysoncloud-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./tysoncloud.nix
+        ];
+      };
     };
-  };
 }
-
